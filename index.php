dd1bbb1342d50b55abd99ac0c588be2240e135ae
Balleno <pedrolorenzovega@gmail.com>
2022-03-29 12:52:05 +0200

    commit martes

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <?php include 'php/conexion.php' ?>
+    <h1>Colaboración de Git</h1>
+    <table>
+        
+    </table>
+</body>
+</html>
